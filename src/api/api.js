7bc3bb03696713db6d7cbd41577ead5717c63012
Jone Baker <iliyinchikartem@gmail.com>
2021-03-30 09:57:20 +0300

    Lesson63 create imitacion DAL

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from "axios";
+
+const instance = axios.create(
+    {
+        withCredentials: true,
+        baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+        headers: {
+            "API-KEY":"bb1bcaee-5836-494b-8da8-f35ce47cf899"
+        }
+    }
+)
+
+export const UserAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => {
+                return response.data
+            })
+    }
+
+}
